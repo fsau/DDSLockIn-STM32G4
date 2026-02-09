d57036b2b482232099320a522e2d99f6696b8e97
Franco Sauvisky <francosauvisky@gmail.com>
2026-02-09 13:47:23 -0300

    damn snprintf

--- modules/utils.c
+++ modules/utils.c
@@ -1,4 +1,5 @@
 #include "utils.h"
+#include <string.h>
 #include <libopencm3/cm3/nvic.h>
 #include <libopencm3/stm32/dma.h>
 
@@ -37,4 +38,56 @@ void dma_channel_enable_irq_with_priority(uint32_t dma_channel, uint8_t priority
     // Set priority and enable
     nvic_set_priority(irq, priority);
     nvic_enable_irq(irq);
+}
+
+char *fmt_f(char *p, float x, int width, int decimals)
+{
+    /* scale = 10^decimals */
+    int32_t scale = 1;
+    for (int i = 0; i < decimals; i++)
+        scale *= 10;
+
+    /* convert with rounding */
+    int32_t v = (int32_t)(x * scale + (x >= 0 ? 0.5f : -0.5f));
+
+    char tmp[24];
+    char *t = tmp;
+
+    if (v < 0) {
+        *t++ = '-';
+        v = -v;
+    }
+
+    int32_t ip = v / scale;
+    int32_t fp = v % scale;
+
+    /* integer part */
+    char ibuf[12];
+    int n = 0;
+    do {
+        ibuf[n++] = '0' + (ip % 10);
+        ip /= 10;
+    } while (ip);
+
+    while (n--)
+        *t++ = ibuf[n];
+
+    if (decimals) {
+        *t++ = '.';
+        for (int32_t d = scale / 10; d; d /= 10) {
+            *t++ = '0' + (fp / d);
+            fp %= d;
+        }
+    }
+
+    int len = t - tmp;
+
+    /* right-align (space padded) */
+    while (len < width) {
+        *p++ = ' ';
+        width--;
+    }
+
+    memcpy(p, tmp, len);
+    return p + len;
 }
--- modules/utils.h
+++ modules/utils.h
@@ -1,6 +1,15 @@
 #pragma once
+
 #include <stdlib.h>
 #include <stdint.h>
 
+/* Device-specific CCM addresses */
+#define CCM_BASE        0x10000000UL
+#define CCM_ALIAS_BASE  0x20005800UL   /* adjust per RM */
+#define CCM_ALIAS_OFFS  (CCM_ALIAS_BASE - CCM_BASE)
+
 // Helper function to enable and set priority for DMA channel
 void dma_channel_enable_irq_with_priority(uint32_t dma_channel, uint8_t priority);
+
+// Helper function for printing floats without snprintf
+char *fmt_f(char *p, float x, int width, int decimals);
